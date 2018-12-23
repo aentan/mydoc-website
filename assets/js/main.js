3637f5030414bf06282cd030e807f27bccfc4410
Aen <hello@aentan.com>
2018-12-24 02:45:19 +0800

    switch to cycle2

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,1 @@
-$(function() {
-
-  $('.mydoc-slider .slides').cycle({
-    pager: '.mydoc-slider-pager',
-    prev: '.mydoc-slider-nav.prev',
-    next: '.mydoc-slider-nav.next',
-    fit: 1,
-    cleartypeNoBg: true
-  });
-
-});
+//

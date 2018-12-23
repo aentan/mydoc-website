944f7beb2599c2b7c93ccda6eb344dec854ffad2
Aen <hello@aentan.com>
2018-12-23 19:25:34 +0800

    Publishing to gh-pages (deploy.sh)

--- assets/js/main.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-
-  $('.mydoc-slider .slides').cycle({
-    pager: '.mydoc-slider-pager',
-    fit: 1,
-    cleartypeNoBg: true
-  });
-
-});
--- content/employers.md
+++
@@ -1,4 +0,0 @@
----
-title: "Employers"
-type: "employers"
----
